4073a75b6273c03d230422661d8fbe8031689173
serpius02 <myojoong@vt.edu>
2025-04-26 16:25:07 +0900

    first commit for project

---
+++ nextjs/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        pretendard: ["Pretendard", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
