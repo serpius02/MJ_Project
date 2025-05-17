f89b861014ac30d0c88e8dd3c2974c16f0317648
serpius02 <myojoong@vt.edu>
2025-05-18 00:22:26 +0900

    authentication-suspense-3

--- nextjs/app/ko/(auth)/forgot-password/confirmation/page.tsx
+++ nextjs/app/ko/(auth)/forgot-password/confirmation/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import ForgotPasswordConfirmationContent from "@/components/(KOR)/(login)/ForgotPasswordConfirmation";
+import ForgotPasswordConfirmationContent from "@/components/(KOR)/(login)/ForgotPasswordConfirmationContent";
 
 // Next.js 15에서는 클라이언트 사이드 라우팅 시 useSearchParams()와 같은 특정 훅들이 Suspense 컴포넌트로 감싸져 있어야 함
 export default function ForgotPasswordConfirmationPage() {
